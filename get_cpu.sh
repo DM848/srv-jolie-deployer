9aef9c25fe3f9152c72f0aaf1dd866cf0f7eadd1
Steffen <klenow@live.dk>
2018-12-20 14:15:46 +0100

    fixed now?

---
+++ get_cpu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+used=$(kubectl describe nodes | grep -E "cpu[ ]*[0-9]+" | grep -o -E "cpu[ ]+[0-9]*" | grep -o -E "[0-9]*")
+total=$(kubectl describe nodes | grep -E "cpu:[ ]*[0-9]+" | grep -v -E "cpu:[ ]*[0-9]+m" | grep -o -E "[0-9]*")
+echo $total $used
