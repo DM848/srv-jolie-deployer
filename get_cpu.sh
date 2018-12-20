5c276d72fe9cbcd0e89cf089ea0279ee02f0dbf1
Steffen <klenow@live.dk>
2018-12-20 14:11:35 +0100

    fixed line endings?

--- get_cpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-useds=$(kubectl describe nodes | grep -E "cpu[ ]*[0-9]+" | grep -o -E "cpu[ ]+[0-9]*" | grep -o -E "[0-9]*")
-total=$(kubectl describe nodes | grep -E "cpu:[ ]*[0-9]+" | grep -v -E "cpu:[ ]*[0-9]+m" | grep -o -E "[0-9]*")
-echo $total $useds
--- get_memory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# used_ki=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+Ki" | grep -o -E "[0-9]+Ki")
-useds=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+[MK]i" | grep -o -E "[0-9]+[MK]i")
-total=$(kubectl get nodes -o jsonpath='{range .items[*]}{.status.allocatable.memory} {end}')
-echo $total $useds
