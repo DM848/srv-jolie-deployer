d13265e816b9db9980b91842e1e93c8754f6861a
Steffen <klenow@live.dk>
2018-12-20 14:06:28 +0100

    maybe fixed #loveWindows

--- get_memory.sh
+++ get_memory.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # used_ki=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+Ki" | grep -o -E "[0-9]+Ki")
-used=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+[MK]i" | grep -o -E "[0-9]+[MK]i")
+useds=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+[MK]i" | grep -o -E "[0-9]+[MK]i")
 total=$(kubectl get nodes -o jsonpath='{range .items[*]}{.status.allocatable.memory} {end}')
-echo $total $used
+echo $total $useds
