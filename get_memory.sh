960d5aeab33b21ab97b11a6c142ff322bef19e9f
Joel Händig <joel.handig94@gmail.com>
2018-12-20 14:21:20 +0100

    newlines

--- get_memory.sh
+++ get_memory.sh
@@ -3,3 +3,4 @@
 used=$(kubectl describe nodes | grep -o -E "memory[ ]*[0-9]+[MK]i" | grep -o -E "[0-9]+[MK]i")
 total=$(kubectl get nodes -o jsonpath='{range .items[*]}{.status.allocatable.memory} {end}')
 echo $total $used
+   
